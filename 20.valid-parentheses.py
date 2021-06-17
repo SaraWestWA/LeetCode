ef2ab41aee17630ca10cdb6b7aa852495ce03ce3
SaraWestWA <64798747+SaraWestWA@users.noreply.github.com>
2021-06-16 20:02:54 -0700

    solved parentheses

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -7,6 +7,31 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
+        # c = []
+        # for char in s:
+        #     if char in ("(", "[", "{"):
+        #         c.append(char)
+        #         print(c)
+        #     elif char[-1] == "(" and char == ")":
+        #         c.pop()
+        #     elif char[-1] == "[" and char == "]":
+        #         c.pop()
+        #     elif char[-1] == "{" and char == "}":
+        #         c.pop()
+        #     else:
+        #         return False
+        
+        # if len(c) == 0:
+        #     return 
+        #     True
+        while len(s) > 0:
+            temp = s
+            s = s.replace('()','')
+            s = s.replace('[]','')
+            s = s.replace('{}','')
+            if temp == s:
+                return False
+        return True
         
 # @lc code=end
 
